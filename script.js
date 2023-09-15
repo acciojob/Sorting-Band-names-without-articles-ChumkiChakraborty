d329041ad51053a31cf12e863252f0bd848f38a4
ChumkiChakraborty <137317244+ChumkiChakraborty@users.noreply.github.com>
2023-09-15 22:21:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 //your code here
 let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
 let ul = touristSpots.map(item => item.replace(/^(a|an|the)\s+/i, ''));
-    let li = ul.sort();
-    console.log(li);
+let li = ul.sort();
+function displayTouristSpots(touristSpots) {
+    let bandList = document.getElementById('band');
+    
+    sortTouristSpots(touristSpots).forEach(name => {
+        let listItem = document.createElement('li');
+        listItem.textContent = name;
+        bandList.appendChild(listItem);
+    });
+}
+
+// Call the function to display the sorted tourist spots
+displayTouristSpots(touristSpots);
