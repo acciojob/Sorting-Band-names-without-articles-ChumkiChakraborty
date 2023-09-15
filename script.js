9302b29e93e48d5be28091b2d2cbf254b7339952
ChumkiChakraborty <137317244+ChumkiChakraborty@users.noreply.github.com>
2023-09-15 22:01:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
 //your code here
-
+let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+let updatedArray = touristSpots.map(item => item.replace(/^(a|an|the)\s+/i, ''));
+    let arr = updatedArray.sort();
+    console.log(arr);
